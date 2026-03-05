c3dc3b464f2fe02223ac0ef8bab05bd7521d6bfd
voicekapp1-ux <voicekapp1@gmail.com>
2026-03-05 03:31:33 +0300

    Contact: fix GitHub link to voicekapp1-ux

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -258,7 +258,7 @@ const Contact: React.FC = () => {
                       <svg className="size-5 fill-current" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path></svg>
                     </a>
                     <a
-                      href="https://github.com/oebitw"
+                                          href="https://github.com/voicekapp1-ux"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="size-11 rounded-xl bg-bg-surface border border-border-subtle flex items-center justify-center text-text-secondary hover:bg-text-muted hover:border-text-muted hover:text-white transition-all duration-300"
